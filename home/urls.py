0e1a8512d71fca8b95b7cc50da076c410d38b22b
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 10:23:06 +0200

    Remove permission to a user functionaity

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ app_name = 'base'
 urlpatterns = [
     path('permissions/', PermissionListView.as_view(), name='permissionList'),
     path('assign-permission/', AssignPermissionView.as_view(), name='assignPermission'),
+    path('remove-permission/', RemovePermissionView.as_view(), name='removePermission'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
