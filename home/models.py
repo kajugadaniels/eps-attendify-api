840a8e2be731cd00a0f7942388b90840b8c499f3
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 13:31:51 +0200

    Add field model

--- home/models.py
+++ home/models.py
@@ -18,3 +18,10 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.name
+
+class Field(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
