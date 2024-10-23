46c20e019a59dbb57b60ad5274ef1180998fb7e1
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 10:27:03 +0200

    Check user permissions

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('permissions/', PermissionListView.as_view(), name='permissionList'),
     path('assign-permission/', AssignPermissionView.as_view(), name='assignPermission'),
     path('remove-permission/', RemovePermissionView.as_view(), name='removePermission'),
+    path('permissions/<int:user_id>/', UserPermissionsView.as_view(), name='userPermissions'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
