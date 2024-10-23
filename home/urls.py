8ac440eb6b9cdf6cffaf6f8fad84a3a5f86e9bcf
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 10:21:13 +0200

    Add assign permission to a user functionality

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ app_name = 'base'
 
 urlpatterns = [
     path('permissions/', PermissionListView.as_view(), name='permissionList'),
+    path('assign-permission/', AssignPermissionView.as_view(), name='assignPermission'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
