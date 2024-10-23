75c553bc268d190ca35b923177060c30b1ba2165
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 10:37:17 +0200

    Add UserSerializer

--- home/serializers.py
+++ home/serializers.py
@@ -6,3 +6,20 @@ class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
         fields = ('id', 'name', 'codename', 'content_type')
+
+class UserSerializer(serializers.ModelSerializer):
+    user_permissions = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = (
+            'id', 'name', 'email', 'phone_number', 'role', 'password', 'user_permissions'
+        )
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User.objects.create_user(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+
+        return user
