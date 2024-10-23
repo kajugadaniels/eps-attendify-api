feffbadc0375403bf098cb7ed25fb5b631b45f9f
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 11:50:26 +0200

    Add Department model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
