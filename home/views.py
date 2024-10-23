8ee40ac5294820387bfa8743cc7310fcd2c26968
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 10:14:34 +0200

    Remove getPermissions route from account to home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from account.serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import GenericAPIView
+from django.core.exceptions import PermissionDenied
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class PermissionListView(generics.ListAPIView):
+    """
+    View to list all permissions. Accessible only to users with appropriate permissions or superusers.
+    """
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    permission_classes = [permissions.IsAuthenticated] # Change to ensure only authenticated users can access
+
+    def get_queryset(self):
+        # Allow superusers unrestricted access
+        if self.request.user.is_superuser:
+            return super().get_queryset()
+
+        # Check if the user has the 'view_permission' permission
+        if not self.request.user.has_perm('auth.view_permission'):
+            raise PermissionDenied({'message': "You do not have permission to view permissions."})
+
+        return super().get_queryset()
