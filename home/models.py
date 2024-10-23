dc10f7384732f647f5bbd887126f3c5011611282
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-23 12:31:10 +0200

    Add day_salary field in department

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
+    day_salary = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
