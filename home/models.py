135ada3dee045f45a2519b4a0a7e99e145060254
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-03 23:39:43 +0200

    Increase characters on the employee model

--- home/models.py
+++ home/models.py
@@ -11,13 +11,13 @@ class Department(models.Model):
         return self.name
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
-    email = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    address = models.CharField(max_length=20, null=True, blank=True)
-    tag_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    nid = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    rssb_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    tag_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    nid = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    rssb_number = models.CharField(max_length=255, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
